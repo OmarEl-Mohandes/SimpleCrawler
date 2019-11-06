0ece84886180c203a4d725d9409fa8c21af38b13
Omar ElMohandes <oelmohan@amazon.com>
2019-11-06 23:47:48 +0000

    Use logger instead of fmt.print to be thread safe

--- Crawler/Crawler.go
+++ Crawler/Crawler.go
@@ -1,8 +1,8 @@
 package Crawler
 
 import (
-	"fmt"
-	"runtime"
+	"log"
+	"os"
 	ds "simpleCrawler/DataStructures"
 	"simpleCrawler/Fetcher"
 	"sync"
@@ -18,7 +18,7 @@ const (
 // Then, it fetches the relative urls based on the baseUrl given, and it'll send all new urls (not cached)
 // to the Queue again (using Out channel).
 // If the worker is Idle for maxWorkerIdleDuration, then the worker will quit.
-func crawl(baseUrl *string, queue *ds.Queue, cache *ds.ConcurrentCache, wg *sync.WaitGroup) {
+func crawl(baseUrl *string, queue *ds.Queue, cache *ds.ConcurrentCache, wg *sync.WaitGroup, logger *log.Logger) {
 	go func() {
 		idleTimer := time.NewTimer(maxWorkerIdleDuration)
 		resetTimer := func() {
@@ -35,7 +35,7 @@ func crawl(baseUrl *string, queue *ds.Queue, cache *ds.ConcurrentCache, wg *sync
 					if _, ok := cache.Load(parentUrl); ok {
 						continue
 					}
-					fmt.Printf("URL found: %v\n", parentUrl)
+					logger.Printf("URL found: %v\n", parentUrl)
 					cache.Store(parentUrl)
 					childrenUrls, _ := Fetcher.FetchRelativeUrlsFromPage(baseUrl, parentUrl)
 					for _, u := range childrenUrls {
@@ -53,13 +53,13 @@ func crawl(baseUrl *string, queue *ds.Queue, cache *ds.ConcurrentCache, wg *sync
 }
 
 func Process(seedUrl *string, maxWorkers int) {
-	runtime.GOMAXPROCS(3)
+	logger := log.New(os.Stdout, "", 0)
 	var cache = ds.NewConcurrentCache()
 	queue := ds.NewQueue(maxWorkers)
 	wg := &sync.WaitGroup{}
 	wg.Add(maxWorkers)
 	for i := 0 ; i < maxWorkers; i ++ {
-		go crawl(seedUrl, queue, cache, wg)
+		go crawl(seedUrl, queue, cache, wg, logger)
 	}
 	queue.In <- *seedUrl
 	wg.Wait()
